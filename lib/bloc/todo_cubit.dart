db7da5dce7f8c8be4ce5d1421cb078a4976843b6
sunny <sunny_ryu@outlook.com>
2022-06-07 21:17:18 +0900

    error change

--- lib/bloc/todo_cubit.dart
+++ lib/bloc/todo_cubit.dart
@@ -7,16 +7,18 @@ class TodoCubit extends Cubit<TodoState> {
   final TodoRepository repository;
 
   TodoCubit({required this.repository}) : super(Empty());
-
-  // cubit은 일반 메소드를 사용해서 이벤트를 호출 할 수 있음
-
+// cubit은 일반 메소드를 사용해서 이벤트를 호출 할 수 있음
   listTodo() async {
     try {
       emit(Loading());
       // cubit의 emit은 yield와 비슷
       final resp = await this.repository.listTodo();
 
-      final todos = resp.map<Todo>((e) => Todo.fromJson(e)).toList();
+      final todos = resp
+          .map<Todo>(
+            (e) => Todo.fromJson(e),
+          )
+          .toList();
 
       emit(Loaded(todos: todos));
     } catch (e) {
@@ -28,25 +30,37 @@ class TodoCubit extends Cubit<TodoState> {
   createTodo(String title) async {
     try {
       if (state is Loaded) {
-        final parseState = (state as Loaded);
+        final parsedState = (state as Loaded);
 
         final newTodo = Todo(
           // 밑에서 서버요청이 끝나고 난 후에 id, createAt은 server가 끝나는 시간으로 바꿔줘야하므로 .. 밑에서 loaded를 한번더 선언하여 맞춤
-          id: parseState.todos[parseState.todos.length - 1].id + 1,
+          id: parsedState.todos[parsedState.todos.length - 1].id + 1,
           title: title,
           createAt: DateTime.now().toString(),
         );
-        // await this.repository보다 newtodo를 빠르게 선언하고자 한다면,
-        final prevTodos = [...parseState.todos];
 
-        final newTodos = [...prevTodos, newTodo];
+        final prevTodos = [
+          ...parsedState.todos,
+        ];
+
+        final newTodos = [
+          ...prevTodos,
+          newTodo,
+        ];
 
         emit(Loaded(todos: newTodos));
+
         final resp = await this.repository.createTodo(newTodo);
         // JSON으로 넘어옴
 
-        emit(Loaded(todos: [...prevTodos, Todo.fromJson(resp)]));
-        // await this.repository.createTodo(newTodo);
+        emit(
+          Loaded(
+            todos: [
+              ...prevTodos,
+              Todo.fromJson(resp),
+            ],
+          ),
+        );
       }
     } catch (e) {
       emit(Error(message: e.toString()));
@@ -58,9 +72,9 @@ class TodoCubit extends Cubit<TodoState> {
       if (state is Loaded) {
         final newTodos = (state as Loaded)
             .todos
-            .where((item) => item.id != item.id)
+            .where((item) => item.id != todo.id)
             .toList();
-        //지우기전에 값을 보여주고 싶어서 whare.tolist사용
+//지우기전에 값을 보여주고 싶어서 whare.tolist사용
         emit(Loaded(todos: newTodos));
 
         await repository.deleteTodo(todo);
